66cded9bdd9d66df5ff03c1a44445d46b500a228
Arka Sadhu <ark.sadhu2904@gmail.com>
2018-07-11 14:00:40 -0700

    Added clr results

--- code/mobile_net.py
+++ code/mobile_net.py
@@ -1,71 +1,69 @@
-import torch
-from collections import OrderedDict
-import pdb
+from tqdm import tqdm
+tqdm.monitor_interval = 0
 
+from fastai.imports import *
+from fastai.transforms import *
+from fastai.conv_learner import *
+from fastai.model import *
+from fastai.dataset import *
+from fastai.sgdr import *
+from fastai.plots import *
+import json
+import pandas as pd
+from sklearn.metrics import *
 
-class mobile_net_model(torch.nn.Module):
-    def __init__(self, config=None):
+def total_model_params(summary):
+    total_params = 0
+    for key,value in summary.items():
+        total_params+=int(summary[key]['nb_params'])
+    print("Total parameters in the model :"+str(total_params))
+    
+    
+class depthwise_block(nn.Module):
+    def __init__(self, in_c, out_c, stride=1):
         super().__init__()
-        self.config = config
-        self.build_model()
-        return
-
-    def conv2d_m(self, F, M, K, S, N):
-        """
-        F is num input features
-        M is num input channels
-        K is kernel size, assumed kx=ky
-        S is stride
-        N is num of output channels
-        """
-        # Need in_channels = out_channels = groups
-        # for the case of depthwise separable conv
-        P = (K + S * F - S - F) // 2
-        conv = torch.nn.Sequential(
-            # Depthwise
-            torch.nn.Conv2d(M, M, K, S, padding=P, groups=M),
-            torch.nn.BatchNorm2d(M),
-            torch.nn.ReLU(),
-            # PointWise
-            torch.nn.Conv2d(M, N, 1, 1),
-            torch.nn.BatchNorm2d(N),
-            torch.nn.ReLU()
-        )
-
-        return conv
-
-    def build_model(self):
-        F = self.config['inp_dim'][0]
-        layers = [32, 64, (128, 2), 128, (256, 2), 256, (512, 2),
-                  512, 512, 512, 512, 512, (1024, 2), 1024]
-        num_classes = self.config['num_classes']
-        self.mobile_net = torch.nn.Sequential()
-        self.mobile_net.add_module('l1_conv_s2', torch.nn.Conv2d(3, 32, 3))
-        self.mobile_net.add_module('bn1', torch.nn.BatchNorm2d(32))
-        for ind, l in enumerate(layers[:-1]):
-            N = layers[ind+1]
-            if type(l) == tuple:
-
-                self.mobile_net.add_module('l'+str(ind)+'_convm_s2',
-                                           self.conv2d_m(F, l[0], 3, 2, N))
-            else:
-                if type(N) == tuple:
-                    N = N[0]
-                self.mobile_net.add_module('l'+str(ind)+'_convm',
-                                           self.conv2d_m(F, l, 3, 1, N))
-
-        self.mobile_net.add_module('l_avg', torch.nn.AvgPool2d(7))
-        self.mobile_net.add_module('l_fc', torch.nn.Linear(1024, num_classes))
-        # self.mobile_net = torch.nn.Sequential(OrderedDict([
-        #     ('l1_conv_s2', torch.nn.Conv2d(3, 32, 3, 2)),
-        #     ('bn1', torch.nn.BatchNorm2d(32)),
-        #     ('l2_convm', self.conv2d_m(F//2, 32, 3, 1, 64)),
-        #     ('l3_convm_s2', self.conv2d_m(F//2, 64, 3, 2, 128)),
-        #     ('l4_convm', self.conv2d_m(F//4, 128, 3, 2, 128)),
-        #     ('l5_convm_s2', self.conv2d_m(F//4, 128, ))
-        # ])
-        # )
-        return
-
+        self.bn1 = nn.BatchNorm2d(in_c)
+        self.conv1 = nn.Conv2d(in_c, in_c, kernel_size=3, 
+                               stride=stride, padding=1,
+                               groups=in_c, bias=False)
+        self.conv2 = nn.Conv2d(in_c, out_c, kernel_size=1,
+                              stride=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_c)
+    
     def forward(self, inp):
-        return self.mobile_net(inp)
+        out = F.relu(self.bn1(self.conv1(inp)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        return out
+    
+class mblnetv1(nn.Module):
+    def __init__(self, block, inc_list, inc_scale, num_blocks_list, stride_list, num_classes):
+        super().__init__()
+        self.num_blocks = len(num_blocks_list)
+        inc_list1 = [o//inc_scale for o in inc_list]
+        self.in_planes = inc_list1[0]
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        
+        lyrs = []
+        for inc, nb, strl in zip(inc_list1[1:], num_blocks_list, stride_list):
+            lyrs.append(self._make_layer(block, inc, nb, strl))
+            
+        self.lyrs = nn.Sequential(*lyrs)
+        self.linear = nn.Linear(inc_list1[-1], num_classes)
+        
+    
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes
+        return nn.Sequential(*layers)
+    
+    def forward(self, inp):
+        out = F.relu(self.bn1(self.conv1(inp)))
+        out = self.lyrs(out)
+        out = F.adaptive_avg_pool2d(out, 1)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return F.log_softmax(out, dim=-1)
